b03fa44b73fb740d21f3172ed860f219d21d5b45
Fernanda Canales <fernanda.canales@prepaibero.mx>
2023-09-25 19:50:35 -0600

    inicial.2

--- calculadora_GIT.c
+++ calculadora_GIT.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 int main
 {
+char operador;
+printf ("Ingresa un operador\n")
+scanf ("%c",&operador);
 
+switch (operador) {
+case '+'
+printf ("es una suma\n");
+break;
+case '-'
+printf ("es una resta\n");
+break;
+case '*'
+printf ("es una multiplicación\n");
+break;
+case '\'
+printf ("es una división\n");
+break;
+default:
+("Error, ingresa un operador válido\n");
 
+}
   return 0;
 }
